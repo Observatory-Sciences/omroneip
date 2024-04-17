e031a8b720f518ec1af0fe5c95c6b336af63e936
Phil Smith <pts@observatorysciences.co.uk>
2024-04-17 15:16:25 +0100

    testing ci

--- .ci-local/install-libplctag.py
+++ .ci-local/install-libplctag.py
@@ -11,8 +11,8 @@ sys.path.append(os.path.join(curdir, '.ci'))
 import cue
 cue.detect_context()
 
-sourcedir = os.path.join(cue.homedir, '.source')
-sdkdir = os.path.join(sourcedir, 'sdk')
+sourcedir = os.path.join(cue.homedir, '.source/libplctag')
+sdkdir = os.path.join(sourcedir, os.environ['LIBPLCTAG'])
 
 if 'LIBPLCTAG' in os.environ:
     with open(os.path.join(curdir, 'configure', 'CONFIG_SITE.local'), 'a') as f:
@@ -23,17 +23,24 @@ if 'LIBPLCTAG' in os.environ:
     except:
         pass
 
-    zip_name = '{0}.zip'.format(os.environ['LIBPLCTAG'])
+    tar_name = '{0}.tar.gz'.format(os.environ['LIBPLCTAG'])
     print('Downloading libplctag {0}'.format(os.environ['LIBPLCTAG']))
     sys.stdout.flush()
-    sp.check_call(['curl', '-fsSL', '--retry', '3', '-o', zip_name,
+    sp.check_call(['curl', '-fsSL', '--retry', '3', '-o', tar_name,
             'https://github.com/libplctag/libplctag/archive/refs/tags/{0}'
-            .format(zip_name)],
+            .format(tar_name)],
             cwd=cue.toolsdir)
     
     if os.path.exists(sdkdir):
         shutil.rmtree(sdkdir)
-    zip_path = os.path.join(cue.toolsdir, zip_name)
-    with zipfile.ZipFile(zip_path, 'r') as zip_ref:
-        zip_ref.extractall(sdkdir)
-    os.remove(zip_path)
+    os.makedirs(sdkdir)
+    tar_path = os.path.join(cue.toolsdir, tar_name)
+    print("Extracting sdk to " + sdkdir)
+    sp.check_call(['tar', '-C', sdkdir, '-xz', '-f', tar_path, '--strip-components', '1'])
+    os.remove(tar_path)
+
+    print("Building sdk with cmake")
+    build = os.path.join(sdkdir, 'build')
+    os.makedirs(build)
+    sp.check_call(["cmake", "..", "-DCMAKE_BUILD_TYPE=Release"],cwd=build)
+    sp.check_call(["make"],cwd=build)
