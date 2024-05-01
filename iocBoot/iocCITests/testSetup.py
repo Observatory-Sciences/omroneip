c450c236e4bf8ce45aae2afb5ec5f94f22844e4f
Phil Smith <pts@observatorysciences.co.uk>
2024-05-01 13:18:31 +0100

    CI improvements

--- iocBoot/iocCITests/testSetup.py
+++ iocBoot/iocCITests/testSetup.py
@@ -16,8 +16,14 @@ class TestSetup:
         #environ["PATH"] = "/home/runner/.cache/base-R3.15.9" + environ.get("PATH")
         self.EPICS_HOST_ARCH = environ.get("EPICS_HOST_ARCH")
         self.EPICS_BASE = environ.get("EPICS_BASE")
-        print("Epics base is: " +  environ.get("IOC_TOP"))
-        print("IOC top is: " + self.EPICS_BASE)
+        if self.EPICS_BASE!=None:
+            print("Epics base is: " + self.EPICS_BASE )
+        else:
+            print("Could not find EPICS base")
+        if environ.get("IOC_TOP") != None:
+            print("IOC top is: " + environ.get("IOC_TOP"))
+        else:
+            print("Could not find IOC top")
         self.IOC_EXECUTABLE = (f"{self.iocPath}/bin/linux-x86_64/omroneipApp")
         self.IOC_CMD = (f"{self.iocPath}/iocBoot/iocCITests/testInt.cmd")
         environ["EPICS_DB_INCLUDE_PATH"] = (f"{self.iocPath}/omroneipApp/db")
