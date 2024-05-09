7ec2a83e9865f9a00ec0c04b3eb481cc30789658
Phil Smith <pts@observatorysciences.co.uk>
2024-05-09 15:31:18 +0100

    First compiling version of new offset calculation system

--- iocBoot/iocTest/st.cmd
+++ iocBoot/iocTest/st.cmd
@@ -10,7 +10,7 @@ omroneipApp_registerRecordDeviceDriver(pdbbase)
 drvOmronEIPConfigure("omronDriver", "10.2.2.57", "18,10.2.2.57","omron-njnx", "2")
 
 #asynSetTraceFile omronDriver 0 asynTrace.out
-#asynSetTraceMask omronDriver 0 0x00FF
+asynSetTraceMask omronDriver 0 0x00FF #everything
 #asynSetTraceMask omronDriver 0 0x0037
 #asynSetTraceMask omronDriver 0 0x0021
 
@@ -26,12 +26,12 @@ drvOmronEIPStructDefine("omronDriver", "iocBoot/iocTest/structDefs.csv")
 #dbLoadRecords("db/testHeatingZones.db", "P=${P}, PORT=omronDriver, POLLER=mediumPoller")
 #dbLoadTemplate("iocBoot/iocTest/testISIS.substitutions")
 #dbLoadRecords("db/testWriteDataTypes.db", "P=${P}, I=1, PORT=omronDriver")
-dbLoadRecords("db/testReadDataTypes.db", "P=${P}, I=1, PORT=omronDriver, POLLER=fastPoller")
+#dbLoadRecords("db/testReadDataTypes.db", "P=${P}, I=1, PORT=omronDriver, POLLER=fastPoller")
 
 #dbLoadRecords("db/testInefficientRead.db", "P=${P}, I=1, R=1:, PORT=omronDriver, POLLER=fastPoller")
 #dbLoadRecords("db/testEfficientRead.db", "P=${P}, I=2, R=2:, PORT=omronDriver, POLLER=fastPoller")
 
-#dbLoadRecords("db/test.db", "P=${P}, PORT=omronDriver, POLLER=mediumPoller")
+dbLoadRecords("db/test.db", "P=${P}, PORT=omronDriver, POLLER=mediumPoller")
 #dbLoadRecords("db/testReadNoPacking.db", "P=${P}, PORT=omronDriver, POLLER=slowPoller") #doesnt work properly atm
 #dbLoadRecords("db/testReadPacking.db", "P=${P}, PORT=omronDriver, POLLER=slowPoller") #doesnt work properly atm
 #dbLoadRecords("db/testWeirdINP.db", "P=${P}, PORT=omronDriver, POLLER=mediumPoller") #doesnt work properly atm
